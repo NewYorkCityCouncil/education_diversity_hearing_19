584c1c753a51fc8374a26affb8145206a1b294b6
Nick Solomon <nsolomon@council.nyc.gov>
2019-04-23 14:48:54 -0400

    small edits

--- code/00_load_data.R
+++ code/00_load_data.R
@@ -6,7 +6,10 @@ library(ggrepel)
 
 school_dems <- read_excel("data/original_data/demographicsnapshot201314to201718public_final.xlsx",
                           sheet = "School") %>%
-  clean_names()
-
-
-
+  clean_names() %>%
+  mutate(school_name = case_when(
+    school_name == "Queens High School for the Sciences at York Colleg" ~ "Queens High School for the Sciences at York College",
+    school_name == "High School for Mathematics, Science and Engineeri" ~ "High School for Mathematics, Science and Engineering",
+    school_name == "Brooklyn Latin School, The" ~ "The Brooklyn Latin School",
+    TRUE ~ school_name
+  ))
